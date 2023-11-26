54f9cf04aa6ee14ff888aa94f1de08d12f2fdabe
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2023-11-26 14:53:13 -0300

    Update screen-2.py

--- screens/screen-2.py
+++ screens/screen-2.py
@@ -4,12 +4,13 @@ import uuid
 from streamlit_card import card
 
 
-def create_card(title):
+def create_card(title, image):
    unique_key = generate_unique_key()
    hasClicked = card(
         title=title,
         text="Compre agora!",
         key=unique_key,
+        image=image,
         on_click=lambda: print("Clicked"),
         styles={
             "card": {
@@ -33,11 +34,11 @@ def generate_unique_key():
 
 def recommend():
     places_to_visit = [
-        ("Paris", 9),
-        ("Roma", 8),
-        ("Londres", 9),
-        ("Tóquio", 7),
-        ("Nova York", 8)
+        ("Paris", "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        ("Roma", "https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        ("Londres", "https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        ("Tóquio", "https://images.unsplash.com/photo-1533050487297-09b450131914?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        ("Nova York", "https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     ]
     return places_to_visit
 
@@ -48,7 +49,7 @@ def recommend_app():
     recommendations = recommend()
     
     for recommendation in recommendations:
-        create_card(f"{recommendation[0]}")
+        create_card(f"{recommendation[0]}", recommendation[1])
         
 
 # Função principal do aplicativo Streamlit
