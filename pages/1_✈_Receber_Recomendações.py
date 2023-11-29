d60b9a33b9eecb027d9d341c192f400fd659fb63
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2023-11-28 22:50:19 -0400

    modification cards page

--- pages/1_✈_Receber_Recomendações.py
+++ pages/1_✈_Receber_Recomendações.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from math import sqrt
-import uuid
-from streamlit_card import card
 
 
 def read_file(filepath):
@@ -12,62 +9,28 @@ def read_file(filepath):
 recommended_places = read_file('common/recommended_places.txt')
 
 
+places = [
+    (recommended_places[0], "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    (recommended_places[1], "https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    (recommended_places[2], "https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    (recommended_places[3], "https://images.unsplash.com/photo-1533050487297-09b450131914?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    (recommended_places[4], "https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    # Add more recommended places as needed
+]
+
 def create_card(title, image):
-   unique_key = generate_unique_key()
-   hasClicked = card(
-        title=title,
-        text="Compre agora!",
-        key=unique_key,
-        image=image,
-        on_click=lambda: print("Clicked"),
-        styles={
-            "card": {
-                "width": "100%",
-                "height": "100px",
-                "border-radius": "10px", 
-                "position": "relative", 
-                "padding": "20px",  
-                "background-color": "#add8e6",
-                "margin-bottom": "0px",
-                "margin-top": "0px"
-            }
-        }
-    )
+    col1, col2 = st.columns([2, 8])
+    with col1:
+        st.image(image, use_column_width=True)
+    with col2:
+        if st.button(f"Compre agora! - {title}"):
+            print(f"Clicked on: {title}")
 
-       
-
-def generate_unique_key():
-    return str(uuid.uuid4())
-
-
-def recommend():
-    places_to_visit = [
-        (recommended_places[0], "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-        (recommended_places[1], "https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-        (recommended_places[2], "https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-        (recommended_places[3], "https://images.unsplash.com/photo-1533050487297-09b450131914?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-        (recommended_places[4], "https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    ]
-    return places_to_visit
-
-# Função para criar o aplicativo Streamlit
-def recommend_app():
-    st.markdown(
-    """
-    <h1 style='text-align: center;'>Recommentrip te sugere os seguintes locais:</h1>
-    """,
-    unsafe_allow_html=True
-)
-
-    recommendations = recommend()
-    
-    for recommendation in recommendations:
-        create_card(f"{recommendation[0]}", recommendation[1])
-        
-
-# Função principal do aplicativo Streamlit
 def main():
-    recommend_app()
+    st.markdown("<h1 style='text-align: center;'>Recommentrip te sugere os seguintes locais:</h1>", unsafe_allow_html=True)
+    
+    for place, image_url in places:
+        create_card(place, image_url)
 
 if __name__ == "__main__":
     main()
